e98291ec16cdb2747f3f203891ab4273236c2271
shihab4513 <shihab4513@gmail.com>
2024-06-17 19:54:40 +0600

    ToDo.jsx further edited. Here diferent way of doing conditional rendering discussed.

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -8,11 +8,58 @@
 // }
 
 // Conditional rendering option 1
+// export default function ToDo({ Task, isDone }) {
+//     if (isDone) {
+//         return <li>Finished : {Task}</li>
+//     }
+//     else {
+//         return <li>Work on : {Task}</li>
+//     }
+// }
+
+// Conditional rendering option 2. using ternary operator
+
+
+// export default function ToDo({ Task, isDone }) {
+//     return(
+//         <li>{isDone?'Finished':'Work on'} : {Task}</li>
+//     );
+// }
+
+
+// Conditional rendering option 3. using '&&' operator
+// export default function ToDo({ Task,isDone }) {
+//         return (
+//  if isDone is true then it print ': Done' .  Basically here 'and' operator check true
+//             <li>{Task} {isDone && ': Done'}</li>
+//         )
+//     }
+
+
+
+// export default function ToDo({ Task,isDone }) {
+//         return (
+//  if isDone is false then it print ': Do it.' . Basically here 'or' operator check false 
+//             <li>{Task} {isDone || ': Do it.'}</li>
+//         )
+//     }
+
+
+// Conditional rendering option 4 . 
 export default function ToDo({ Task, isDone }) {
+
+    let listItem;
     if (isDone) {
-        return <li>Finished : {Task}</li>
+        listItem = <li>Finished : {Task}</li>
     }
     else {
-        return <li>Work on : {Task}</li>
+        listItem = <li>Work on : {Task}</li>
     }
+    return listItem;
 }
+
+
+
+
+
+
